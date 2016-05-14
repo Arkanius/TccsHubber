310b3438b96f2dd73013406630d29390e5b52dd0
Victor Gomes <vtr.gomes@hotmail.com>
2016-05-14 01:45:57 -0300

    editar curso

---
+++ resources/views/admin/editarcurso.blade.php
@@ -0,0 +1,54 @@
+@extends('admin.layout')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Editar Curso</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/editar-curso') }}">
+                        {!! csrf_field() !!}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Nome</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="name" value="{{ $course->name }}" required>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('coordinator') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Coordenador</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="coordinator" value="{{ $course->coordinator }}" required>
+
+                                @if ($errors->has('coordinator'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('coordinator') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <input type="hidden" value="{{ $course->id }}" name="course_id">
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">Salvar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
